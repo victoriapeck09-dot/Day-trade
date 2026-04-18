fdfd7e056634cc1c4b3fd5cb09d4a7f4b64a08a8
Developer <developer@example.com>
2026-04-18 13:04:03 -0600

    Fix paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Day-trade/',
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
   },
 })
